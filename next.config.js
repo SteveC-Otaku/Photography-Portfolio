3e6d87d2746a130fd13941ebd36b013a5fffebcf
SteveC-Otaku <stevechen.otaku@qq.com>
2026-02-27 13:56:09 +1100

    conditional

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: '/Photography-Portfolio',
-  assetPrefix: '/Photography-Portfolio',
+  basePath: isProd ? '/Photography-Portfolio' : undefined,
+  assetPrefix: isProd ? '/Photography-Portfolio' : undefined,
 };
 
 module.exports = nextConfig;
