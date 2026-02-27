4fe242f5674b106ce160b6b463dee2491b212e29
SteveC-Otaku <stevechen.otaku@qq.com>
2026-02-27 13:35:26 +1100

    pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  basePath: '/Photography-Portfolio',
+  assetPrefix: '/Photography-Portfolio',
 };
 
 module.exports = nextConfig;
