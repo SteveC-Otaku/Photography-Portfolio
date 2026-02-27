ca55f2f28b263587641c5f327718beae9f6a33b6
SteveC-Otaku <stevechen.otaku@qq.com>
2026-02-27 13:18:25 +1100

    fix

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repoBasePath = '/Photography-Portfolio';
-
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: isProd ? repoBasePath : undefined,
-  assetPrefix: isProd ? repoBasePath : undefined,
 };
 
 module.exports = nextConfig;
